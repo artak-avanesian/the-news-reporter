ff4d4f66b5dfbcb046710b8507cf8cfcf9042df2
Artak <artak.avanesyan93@gmail.com>
2018-12-15 20:21:50 +0200

    add function like to Favourites

--- src/common/features/Favourites/Favourites.js
+++ src/common/features/Favourites/Favourites.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { keys } from 'lodash'
 import articlesBlock, { getWorldNewsMap } from '../../../App/Main/MainContent/MainNewsContent/WorldNews/Articles/articlesBlock'
 import FavouriteArticle from './FavouriteArticle/FavouriteArticle'
 import './Favourites.css'
@@ -8,27 +7,29 @@ const Favourites = ({
     isLiked,
     favouritesMap = getWorldNewsMap(articlesBlock)
 }) => {
+    const like = () => {
+        var likeActiveArray = [];
+        for (var key in isLiked) {
+            if (isLiked[key] === true) likeActiveArray.push(key);
+            else {
+                for (var i = 0; i < likeActiveArray.length; i++) {
+                    if (key === likeActiveArray[i]) likeActiveArray.splice(i, 1);
+                }
+            }
+        }
+        return likeActiveArray;
+    }
     return (
         <div className='favourites-articles'>
         {
-            // () => {
-            //     var likeActiveArray = [];
-            //     for (var key in isLiked) {
-            //         if (isLiked[key] === true) likeActiveArray.push(key);
-            //         else {
-            //             for (var i = 0; i < likeActiveArray.length; i++) {
-            //                 if (key === likeActiveArray[i]) likeActiveArray.splice(i, 1);
-            //             }
-            //         }
-            //     }
-                keys(isLiked).map((likeId) => (
-                    <div key={likeId}>
-                        <FavouriteArticle
-                            favouriteArticleImage={favouritesMap[likeId].img_src}
-                            favouriteArticleTitle={favouritesMap[likeId].title}
-                            favouriteArticleDescription={favouritesMap[likeId].description}
-                        />
-                    </div>
+            like().map((likeId) => (
+                <div key={likeId}>
+                    <FavouriteArticle
+                        favouriteArticleImage={favouritesMap[likeId].img_src}
+                        favouriteArticleTitle={favouritesMap[likeId].title}
+                        favouriteArticleDescription={favouritesMap[likeId].description}
+                    />
+                </div>
             ))
         }
         </div>
