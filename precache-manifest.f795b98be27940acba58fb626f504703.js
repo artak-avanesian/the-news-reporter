a95d0ab2bbab5e46cbbb69cab0ff6b78e6bf4b09
Artak <artak.avanesyan93@gmail.com>
2019-03-19 17:24:14 +0200

    Updates

---
+++ precache-manifest.f795b98be27940acba58fb626f504703.js
@@ -0,0 +1,462 @@
+self.__precacheManifest = [
+  {
+    "revision": "28aaabfa525563ebc25ae109982b2414",
+    "url": "/the-news-reporter/static/media/news-menu-business-4.28aaabfa.jpg"
+  },
+  {
+    "revision": "11b7df0e12089591b94e",
+    "url": "/the-news-reporter/static/css/main.fa81f3ee.chunk.css"
+  },
+  {
+    "revision": "2dc4f50d9ad8791fe868",
+    "url": "/the-news-reporter/static/js/1.2dc4f50d.chunk.js"
+  },
+  {
+    "revision": "f11ddd391f6b283a36b3",
+    "url": "/the-news-reporter/static/js/runtime~main.f11ddd39.js"
+  },
+  {
+    "revision": "9b35e585afb44db4b1f456bd668a68ac",
+    "url": "/the-news-reporter/static/media/twitter.9b35e585.jpg"
+  },
+  {
+    "revision": "f6615d2686b08adeb8dbeaa2bd6ce670",
+    "url": "/the-news-reporter/static/media/fb.f6615d26.jpg"
+  },
+  {
+    "revision": "4d4408f076e00d71a6ece6e50c6fbdf6",
+    "url": "/the-news-reporter/static/media/wi-fi.4d4408f0.jpg"
+  },
+  {
+    "revision": "c0017b96f74738f19ffbdd1e6bf0188e",
+    "url": "/the-news-reporter/static/media/fb-likes.c0017b96.jpg"
+  },
+  {
+    "revision": "d622c049cd33ad2c47e91e01d05b8d3f",
+    "url": "/the-news-reporter/static/media/counter.d622c049.jpg"
+  },
+  {
+    "revision": "6352363810f8cc5462ee37b0b7d01264",
+    "url": "/the-news-reporter/static/media/search.63523638.jpg"
+  },
+  {
+    "revision": "1a48325e74e28336e32636d512a24747",
+    "url": "/the-news-reporter/static/media/news-3.1a48325e.jpg"
+  },
+  {
+    "revision": "e8d4988d352bda3172674fe6bc8b3460",
+    "url": "/the-news-reporter/static/media/news-2.e8d4988d.jpeg"
+  },
+  {
+    "revision": "61678b400aebf4b00508478851724341",
+    "url": "/the-news-reporter/static/media/news-1.61678b40.jpeg"
+  },
+  {
+    "revision": "9a56a0ce7c211cce3d0606bbf71eb21d",
+    "url": "/the-news-reporter/static/media/news-show.9a56a0ce.jpg"
+  },
+  {
+    "revision": "9a56a0ce7c211cce3d0606bbf71eb21d",
+    "url": "/the-news-reporter/static/media/news-show-small.9a56a0ce.jpg"
+  },
+  {
+    "revision": "15bdf378d5c689d654e4c4bb58258789",
+    "url": "/the-news-reporter/static/media/world-news-1.15bdf378.jpeg"
+  },
+  {
+    "revision": "3da6d7f9439fbde5646cf2b76592f1bd",
+    "url": "/the-news-reporter/static/media/world-news-2.3da6d7f9.jpg"
+  },
+  {
+    "revision": "042040b4b72b0e55f83ad837e8f40ae5",
+    "url": "/the-news-reporter/static/media/world-news-3.042040b4.jpg"
+  },
+  {
+    "revision": "748c08a18151ddce29afb815990884b4",
+    "url": "/the-news-reporter/static/media/latest-articles-1.748c08a1.jpg"
+  },
+  {
+    "revision": "510e587df46996c8b250b5eba2ee7d2a",
+    "url": "/the-news-reporter/static/media/latest-articles-2.510e587d.jpg"
+  },
+  {
+    "revision": "fd51e42cec27cbf8df878b0f6be41ab0",
+    "url": "/the-news-reporter/static/media/latest-articles-3.fd51e42c.jpg"
+  },
+  {
+    "revision": "f50ff93608833ba34375bb9a749ad907",
+    "url": "/the-news-reporter/static/media/gallery-1.f50ff936.jpg"
+  },
+  {
+    "revision": "d1b6b72695a9582e5c29e18ffc0f7701",
+    "url": "/the-news-reporter/static/media/gallery-4.d1b6b726.jpg"
+  },
+  {
+    "revision": "47ad1878ea94153d996f5a08fc31c2c8",
+    "url": "/the-news-reporter/static/media/gallery-6.47ad1878.jpg"
+  },
+  {
+    "revision": "e4ff20a7111a150a3a590cb684ebbefb",
+    "url": "/the-news-reporter/static/media/gallery-5.e4ff20a7.jpg"
+  },
+  {
+    "revision": "d627d937b9e4ccfd64c6be5a23b5a481",
+    "url": "/the-news-reporter/static/media/gallery-2.d627d937.jpg"
+  },
+  {
+    "revision": "9fad4f43440e406ac60c4c29f7e4112d",
+    "url": "/the-news-reporter/static/media/gallery-3.9fad4f43.jpg"
+  },
+  {
+    "revision": "2351bffdaebe13354ef9264a900c17b2",
+    "url": "/the-news-reporter/static/media/editorial-1.2351bffd.jpg"
+  },
+  {
+    "revision": "0c7960f4d0bb14b29af87149e35489e9",
+    "url": "/the-news-reporter/static/media/editorial-2.0c7960f4.jpg"
+  },
+  {
+    "revision": "9a8b6e53d7821ce4cc5f39912181d261",
+    "url": "/the-news-reporter/static/media/editorial-3.9a8b6e53.jpg"
+  },
+  {
+    "revision": "6619f4b9526763d196b7ed8edb2dba78",
+    "url": "/the-news-reporter/static/media/editorial-4.6619f4b9.jpg"
+  },
+  {
+    "revision": "58d377f32ecaf361c8843e803da1cf69",
+    "url": "/the-news-reporter/static/media/video-2.58d377f3.jpg"
+  },
+  {
+    "revision": "91327a4b3352ffb2309f10ec965e0414",
+    "url": "/the-news-reporter/static/media/video-4.91327a4b.jpg"
+  },
+  {
+    "revision": "9bc93cc0433c02a0917d0cb233bda2ff",
+    "url": "/the-news-reporter/static/media/video-1.9bc93cc0.jpg"
+  },
+  {
+    "revision": "5c9d4caf0e19c07cea02dde2623dbd89",
+    "url": "/the-news-reporter/static/media/ad-1.5c9d4caf.jpg"
+  },
+  {
+    "revision": "1d2b3f08db902908598827241eed38ff",
+    "url": "/the-news-reporter/static/media/desk-3.1d2b3f08.jpg"
+  },
+  {
+    "revision": "bb49bbf564ab7bf185402cfcbd3a980e",
+    "url": "/the-news-reporter/static/media/desk-2.bb49bbf5.jpg"
+  },
+  {
+    "revision": "f28d1f5d08ad5eaa8ab180f56916b386",
+    "url": "/the-news-reporter/static/media/desk-1.f28d1f5d.jpg"
+  },
+  {
+    "revision": "40ced2cdeb94cf5fbdcb1db822ff6357",
+    "url": "/the-news-reporter/static/media/news-menu-world-news-6.40ced2cd.jpg"
+  },
+  {
+    "revision": "9529274cb9161c92c3efb4e3809eab82",
+    "url": "/the-news-reporter/static/media/news-menu-world-news-5.9529274c.jpg"
+  },
+  {
+    "revision": "9a2d0f7fe6b3ac63b95ea5cbc79fc402",
+    "url": "/the-news-reporter/static/media/news-menu-world-news-2.9a2d0f7f.jpg"
+  },
+  {
+    "revision": "f667b9730944c304d61ab7e30fd90875",
+    "url": "/the-news-reporter/static/media/news-menu-world-news-1.f667b973.jpg"
+  },
+  {
+    "revision": "2f7260f322853faa74de6ddd4cfa04a6",
+    "url": "/the-news-reporter/static/media/news-menu-world-news-4.2f7260f3.jpg"
+  },
+  {
+    "revision": "ee4e2949ba152ec6643f4d0113631ac0",
+    "url": "/the-news-reporter/static/media/news-menu-world-news-3.ee4e2949.jpg"
+  },
+  {
+    "revision": "e6049c83651661d84778579ef5f6b3d3",
+    "url": "/the-news-reporter/static/media/news-menu-tech-4.e6049c83.jpg"
+  },
+  {
+    "revision": "12bff2d48fdae36c99089854b6736404",
+    "url": "/the-news-reporter/static/media/news-menu-tech-3.12bff2d4.jpg"
+  },
+  {
+    "revision": "7d3e9ade469c2a1b76043f256714dffb",
+    "url": "/the-news-reporter/static/media/news-menu-tech-5.7d3e9ade.jpg"
+  },
+  {
+    "revision": "4a0e3450565e94636981bdcea0f57cf4",
+    "url": "/the-news-reporter/static/media/news-menu-tech-2.4a0e3450.jpg"
+  },
+  {
+    "revision": "64d9ac814b72db3823a1141e8ff415b1",
+    "url": "/the-news-reporter/static/media/news-menu-tech-1.64d9ac81.gif"
+  },
+  {
+    "revision": "6bd0aa901e88ccfc5d646682cb969f38",
+    "url": "/the-news-reporter/static/media/news-menu-sports-3.6bd0aa90.jpg"
+  },
+  {
+    "revision": "f9ee99c562d13747b5cf0f115897fe70",
+    "url": "/the-news-reporter/static/media/news-menu-sports-5.f9ee99c5.jpg"
+  },
+  {
+    "revision": "13bfe4dc54ad9821f7bd3bee84e5150e",
+    "url": "/the-news-reporter/static/media/news-menu-sports-6.13bfe4dc.jpg"
+  },
+  {
+    "revision": "319b6c83f3e2200d8cd64d8edf570cb9",
+    "url": "/the-news-reporter/static/media/news-menu-sports-1.319b6c83.jpg"
+  },
+  {
+    "revision": "2b7f6c3881d85ae2ccabe31c0c47f6cb",
+    "url": "/the-news-reporter/static/media/news-menu-sports-4.2b7f6c38.jpg"
+  },
+  {
+    "revision": "5b24d0cb8509928504d35ad322de361b",
+    "url": "/the-news-reporter/static/media/news-menu-sports-2.5b24d0cb.jpg"
+  },
+  {
+    "revision": "cdc5718715ded4c305ee210896d48ca1",
+    "url": "/the-news-reporter/static/media/news-menu-business-1.cdc57187.jpg"
+  },
+  {
+    "revision": "7d193333e031ea1b998fb05b514b8782",
+    "url": "/the-news-reporter/static/media/news-menu-business-3.7d193333.jpg"
+  },
+  {
+    "revision": "11b7df0e12089591b94e",
+    "url": "/the-news-reporter/static/js/main.11b7df0e.chunk.js"
+  },
+  {
+    "revision": "916268d4c22cdc79b333ffdc17e50d36",
+    "url": "/the-news-reporter/static/media/news-menu-business-2.916268d4.png"
+  },
+  {
+    "revision": "5ca7e818ef4b1ccca86919af7eed52be",
+    "url": "/the-news-reporter/static/media/news-menu-culture-4.5ca7e818.jpg"
+  },
+  {
+    "revision": "293d52fcfddf0e817d729679b52cbbb3",
+    "url": "/the-news-reporter/static/media/news-menu-culture-2.293d52fc.jpg"
+  },
+  {
+    "revision": "29e35fc9be066747153212c7c4c02afd",
+    "url": "/the-news-reporter/static/media/news-menu-culture-6.29e35fc9.jpg"
+  },
+  {
+    "revision": "bfb92300a0f6d4fdab911ee3a0ed304d",
+    "url": "/the-news-reporter/static/media/news-menu-culture-1.bfb92300.jpg"
+  },
+  {
+    "revision": "46c053fb313787b92e57e484ba3fa222",
+    "url": "/the-news-reporter/static/media/news-menu-culture-3.46c053fb.jpg"
+  },
+  {
+    "revision": "6904e35db93a8656f6b7e109c70aac8f",
+    "url": "/the-news-reporter/static/media/news-menu-culture-5.6904e35d.jpg"
+  },
+  {
+    "revision": "43e929b82501487da1f34e5a17ec8a79",
+    "url": "/the-news-reporter/static/media/news-menu-music-1.43e929b8.jpg"
+  },
+  {
+    "revision": "f091ff3ff06799013b9f5a845b08d05d",
+    "url": "/the-news-reporter/static/media/news-menu-music-3.f091ff3f.jpg"
+  },
+  {
+    "revision": "ed5d1bb074b8394bae647c6767dde297",
+    "url": "/the-news-reporter/static/media/news-menu-music-2.ed5d1bb0.jpg"
+  },
+  {
+    "revision": "c66b703b5504e482de84a436d66cb4b6",
+    "url": "/the-news-reporter/static/media/news-menu-music-4.c66b703b.jpg"
+  },
+  {
+    "revision": "7f4ce26d5bd9bc14c8461f25fdef9f24",
+    "url": "/the-news-reporter/static/media/news-menu-entertainment-2.7f4ce26d.jpg"
+  },
+  {
+    "revision": "595ee1cac184c0d1d299125fc9801b38",
+    "url": "/the-news-reporter/static/media/news-menu-entertainment-3.595ee1ca.jpg"
+  },
+  {
+    "revision": "be27e5c31a1d9d1728800058a82fcf2b",
+    "url": "/the-news-reporter/static/media/news-menu-entertainment-1.be27e5c3.jpg"
+  },
+  {
+    "revision": "6f64817beb32c99044bdc7d0729ee72a",
+    "url": "/the-news-reporter/static/media/news-menu-movies-4.6f64817b.jpg"
+  },
+  {
+    "revision": "c3e0a6e4e1c36a873ef754370f373c3d",
+    "url": "/the-news-reporter/static/media/news-menu-movies-2.c3e0a6e4.jpg"
+  },
+  {
+    "revision": "63482933efdab893b9824adc721b6432",
+    "url": "/the-news-reporter/static/media/news-menu-movies-5.63482933.jpg"
+  },
+  {
+    "revision": "6baf62f43d7a843f99795cc9929122b9",
+    "url": "/the-news-reporter/static/media/news-menu-movies-6.6baf62f4.jpg"
+  },
+  {
+    "revision": "f1bef581a41ac7c53cbd5380da9bbd44",
+    "url": "/the-news-reporter/static/media/news-menu-movies-3.f1bef581.jpg"
+  },
+  {
+    "revision": "adb5d69b87942b46d5e9b4457df6a73e",
+    "url": "/the-news-reporter/static/media/news-menu-movies-1.adb5d69b.jpg"
+  },
+  {
+    "revision": "6bc39f274772cf34e491e740dd9e54b1",
+    "url": "/the-news-reporter/static/media/news-menu-books-4.6bc39f27.jpg"
+  },
+  {
+    "revision": "7a6f19d2ae169f6c5d6ffbb79825d467",
+    "url": "/the-news-reporter/static/media/news-menu-books-2.7a6f19d2.jpg"
+  },
+  {
+    "revision": "3892f5ecb93ceee330ae2e2c293db0c4",
+    "url": "/the-news-reporter/static/media/news-menu-books-1.3892f5ec.jpg"
+  },
+  {
+    "revision": "4b245e786072cb834f65b07eb2628552",
+    "url": "/the-news-reporter/static/media/news-menu-books-5.4b245e78.jpg"
+  },
+  {
+    "revision": "6c6805a1d3691932280efccb3ae7a1fb",
+    "url": "/the-news-reporter/static/media/news-menu-books-6.6c6805a1.jpg"
+  },
+  {
+    "revision": "fc9f6e5e2baf4d84c62f205998a87216",
+    "url": "/the-news-reporter/static/media/news-menu-books-3.fc9f6e5e.jpg"
+  },
+  {
+    "revision": "33ebfaa30df8c5db18bf34869193c11e",
+    "url": "/the-news-reporter/static/media/news-menu-science-3.33ebfaa3.jpg"
+  },
+  {
+    "revision": "d23e652b6d7591eb4c0c2a066f80671a",
+    "url": "/the-news-reporter/static/media/news-menu-science-4.d23e652b.jpg"
+  },
+  {
+    "revision": "7f01ec9ccb153d780faa83fe12f93101",
+    "url": "/the-news-reporter/static/media/news-menu-science-1.7f01ec9c.jpg"
+  },
+  {
+    "revision": "ba1a28c6c2e32003ce5a802e2d7b9c64",
+    "url": "/the-news-reporter/static/media/news-menu-science-5.ba1a28c6.jpg"
+  },
+  {
+    "revision": "44dd54355005f6b0c792a960376ff26f",
+    "url": "/the-news-reporter/static/media/news-menu-science-2.44dd5435.jpg"
+  },
+  {
+    "revision": "78b36c12a44fcbd5e2e5bf632e57253f",
+    "url": "/the-news-reporter/static/media/galleryImg-12.78b36c12.jpg"
+  },
+  {
+    "revision": "7a925c3edbb5b63c880758e920088264",
+    "url": "/the-news-reporter/static/media/galleryImg-16.7a925c3e.jpg"
+  },
+  {
+    "revision": "2331869d833d47e757d05bd59e7209e8",
+    "url": "/the-news-reporter/static/media/galleryImg-17.2331869d.jpg"
+  },
+  {
+    "revision": "a84ff8af33f1c03d9fe2bd97f3d56be4",
+    "url": "/the-news-reporter/static/media/galleryImg-1.a84ff8af.jpg"
+  },
+  {
+    "revision": "56e5dff18fa95e713893f93762945e1e",
+    "url": "/the-news-reporter/static/media/galleryImg-3.56e5dff1.jpg"
+  },
+  {
+    "revision": "12ec7d3efd1e4403e8484d0a05ef3bde",
+    "url": "/the-news-reporter/static/media/galleryImg-2.12ec7d3e.jpg"
+  },
+  {
+    "revision": "a6d51c60087313b93cff8079033c5c12",
+    "url": "/the-news-reporter/static/media/galleryImg-4.a6d51c60.jpg"
+  },
+  {
+    "revision": "847bd5e34ba0c66ce518dc73d904a44f",
+    "url": "/the-news-reporter/static/media/galleryImg-5.847bd5e3.jpg"
+  },
+  {
+    "revision": "2eb418112d4b2b745fa62142f3f1e3a1",
+    "url": "/the-news-reporter/static/media/galleryImg-13.2eb41811.jpg"
+  },
+  {
+    "revision": "320b0cc0818b76604477660e03ff9329",
+    "url": "/the-news-reporter/static/media/galleryImg-20.320b0cc0.jpg"
+  },
+  {
+    "revision": "3b8937341ddad69fc844524d3e7c6e90",
+    "url": "/the-news-reporter/static/media/galleryImg-6.3b893734.jpg"
+  },
+  {
+    "revision": "aff0d1bfab233620e03a1555266af768",
+    "url": "/the-news-reporter/static/media/galleryImg-7.aff0d1bf.jpg"
+  },
+  {
+    "revision": "552fbc8f35fb4aab7c50fc8d8bdb4488",
+    "url": "/the-news-reporter/static/media/galleryImg-15.552fbc8f.jpg"
+  },
+  {
+    "revision": "ffbe7816dfd7222a8968564252f5f04f",
+    "url": "/the-news-reporter/static/media/galleryImg-11.ffbe7816.jpg"
+  },
+  {
+    "revision": "a07b373584a40b1a3b34f290082f956d",
+    "url": "/the-news-reporter/static/media/galleryImg-10.a07b3735.jpg"
+  },
+  {
+    "revision": "140fed504701656a7bc82261e572e30b",
+    "url": "/the-news-reporter/static/media/galleryImg-14.140fed50.jpg"
+  },
+  {
+    "revision": "bd9d74289654d1c6f5c4c589de4d5b03",
+    "url": "/the-news-reporter/static/media/galleryImg-8.bd9d7428.jpg"
+  },
+  {
+    "revision": "cfc64dd605a1351d410c34adf552f1fc",
+    "url": "/the-news-reporter/static/media/galleryImg-18.cfc64dd6.jpg"
+  },
+  {
+    "revision": "2ffee371aadac971d5bc42156bb22599",
+    "url": "/the-news-reporter/static/media/galleryImg-9.2ffee371.jpg"
+  },
+  {
+    "revision": "d0c45068c42acaf49673cacae77120fa",
+    "url": "/the-news-reporter/static/media/galleryImg-19.d0c45068.jpg"
+  },
+  {
+    "revision": "5a2939e756ee1631b97e9d7ddc2f899b",
+    "url": "/the-news-reporter/static/media/ad-2.5a2939e7.jpg"
+  },
+  {
+    "revision": "d5ec5998c19ff40f34c6dea3c2254bac",
+    "url": "/the-news-reporter/static/media/Glyphter.d5ec5998.ttf"
+  },
+  {
+    "revision": "6f97ba9b5e6ef7c13b6f6efd04189f6d",
+    "url": "/the-news-reporter/static/media/Glyphter2.6f97ba9b.ttf"
+  },
+  {
+    "revision": "3ef73ff48d36ad769ecdd8ad805dda61",
+    "url": "/the-news-reporter/static/media/Bebas.3ef73ff4.TTF"
+  },
+  {
+    "revision": "c9dd124d0a40e22f7c30b67f396ea834",
+    "url": "/the-news-reporter/static/media/lines.c9dd124d.jpg"
+  },
+  {
+    "revision": "429af2b8378d06572f66acf61a05b369",
+    "url": "/the-news-reporter/index.html"
+  }
+];
